da922b650b53076816737011746ad7f10e79b24f
YRAL375 <vvoitkus@mail.ru>
2020-07-20 11:04:32 +0300

    useEffect

--- hookssandbox/src/index.js
+++ hookssandbox/src/index.js
@@ -1,20 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-    return (
-        <MyContext.Provider value={'Hello context!'}>
-            <Child />
-        </MyContext.Provider>
-    );
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    if (visible) {
+        return (
+            <div>
+                <button onClick={() => setValue((v) => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>Hide</button>
+                <HookCounter value={value}/>
+                <Notification />
+            </div>
+        )
+    } else {
+        return <button onClick={() => setVisible(true)}>Show</button>
+    }
 };
 
-const Child = () => {
-      const value = useContext(MyContext);
+const HookCounter = ({ value }) => {
+    useEffect(() => {
+        console.log('mount');
 
-      return <div>{value}</div>
+        return () => console.log('unmount')
+    }, []); //componentDidMount componentWillUnmount
+
+    useEffect(() => {console.log("update")}); //componentDidUpdate
+
+    return <div>{value}</div>
+};
+
+const Notification = () => {
+   const [isNotificationShown, setVisible] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setVisible(false), 1500);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    let output = '';
+
+    if (isNotificationShown) {
+        output = (
+            <div>Hello</div>
+        );
+    }
+
+    return output;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
