7916a1be7d2c5d17449468c967185527f313de1a
YRAL375 <vvoitkus@mail.ru>
2020-07-20 23:08:49 +0300

    useEffect with fetch api

--- hookssandbox/src/index.js
+++ hookssandbox/src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
 
     if (visible) {
@@ -10,13 +10,30 @@ const App = () => {
             <div>
                 <button onClick={() => setValue((v) => v + 1)}>+</button>
                 <button onClick={() => setVisible(false)}>Hide</button>
-                <HookCounter value={value}/>
-                <Notification />
+                <PlanetInfo id={value}/>
             </div>
         )
     } else {
         return <button onClick={() => setVisible(true)}>Show</button>
     }
+};
+
+const PlanetInfo = ({id}) => {
+    const [name, setName] = useState(null);
+
+    useEffect(() => {
+        let cancelled = false;
+
+        fetch(`https://swapi.dev/api/planets/${id}`)
+                .then(response => response.json())
+                .then(data => !cancelled && setName(data.name));
+
+        return () => cancelled = true;
+        }, [id]);
+
+      return (
+          <div>{id} - Planet {name}</div>
+      );
 };
 
 const HookCounter = ({ value }) => {
