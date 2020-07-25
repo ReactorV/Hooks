fa5baa7517a34aad3b6094d5a093c4c56cbe9809
YRAL375 <vvoitkus@mail.ru>
2020-07-25 14:01:32 +0300

    useReducer hook

--- hookssandbox/src/index.js
+++ hookssandbox/src/index.js
@@ -1,88 +1,30 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [value, setValue] = useState(1);
-    const [visible, setVisible] = useState(true);
+    const initialState = {
+        count: 0
+    };
 
-    if (visible) {
-        return (
-            <div>
-                <button onClick={() => setValue((v) => v + 1)}>
-                    +
-                </button>
-                <button onClick={() => setVisible(false)}>
-                    Hide
-                </button>
-                <PlanetInfo id={value}/>
-            </div>
-        )
-    } else {
-        return (
-            <button onClick={() => setVisible(true)}>
-                Show
-            </button>
-        )
-    }
-};
-
-const getPlanet = (id) => {
-    return fetch(`https://swapi.dev/api/planets/${id}`)
-        .then(response => response.json())
-        .then(data => data);
-};
-
-const useRequest = (request) => {
-    const initialState = useMemo(() => ({
-        data: null,
-        loading: true,
-        error: null
-    }), []);
-
-    const [dataState, setDataState] = useState(initialState);
-
-    useEffect(() => {
-        setDataState(initialState);
-
-        let cancelled = false;
-
-        request()
-            .then(data => !cancelled && setDataState({
-                data,
-                loading: false,
-                error: null
-            }))
-            .catch(error => !cancelled && setDataState({
-                data: null,
-                loading: false,
-                error
-            }));
-
-        return () => cancelled = true;
-    }, [ request ]);
-
-    return dataState;
-};
-
-const usePlanetInfo = (id) => {
-    const request = useCallback( () => getPlanet(id), [ id ]);
-
-    return useRequest(request);
-};
-
-const PlanetInfo = ({id}) => {
-    const { data, loading, error } = usePlanetInfo(id);
-
-    if (loading) {
-        return <div>Loading...</div>
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'increment':
+                return {count: state.count + 1};
+            case 'decrement':
+                return {count: state.count -1};
+            default:
+                throw new Error();
+        }
     }
 
-    if (error) {
-        return <div>Something is wrong... </div>
-    }
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
-        <div>{id} - Planet {data.name}</div>
+        <>
+            Count: {state.count}
+            <button onClick={() => dispatch({type: 'increment'})}>+</button>
+            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+        </>
     );
 };
 
