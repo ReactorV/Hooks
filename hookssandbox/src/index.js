9a12061aeed60e77d82c7182fa6f4a13b5d34a30
YRAL375 <vvoitkus@mail.ru>
2020-07-26 14:18:18 +0300

    useRef hook

--- hookssandbox/src/index.js
+++ hookssandbox/src/index.js
@@ -1,43 +1,17 @@
-import React, { useReducer } from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const initialCount = {
-        count: 0
-    };
-
-    function init({ count }) {
-        return {
-            count
-        }
-    }
-
-    function reducer(state, action) {
-        switch (action.type) {
-            case 'increment':
-                return {count: state.count + 1};
-            case 'decrement':
-                return {count: state.count -1};
-            case 'reset':
-                return init(action.payload);
-            default:
-                throw new Error();
-        }
-    }
-
-    const [state, dispatch] = useReducer(reducer, initialCount, init);
+    const inputEl = useRef(null);
+    const onButtonClick = () => inputEl.current.focus();
 
     return (
         <>
-            Count: {state.count}
-            <button onClick={() => dispatch({type: 'increment'})}>+</button>
-            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-            <button
-                onClick={() => dispatch({type: 'reset', payload: initialCount})}>
-                Reset
-            </button>
+            <input ref={inputEl} type="text" />
+            <button onClick={onButtonClick}>Установить фокус на поле ввода</button>
         </>
     );
 };
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
